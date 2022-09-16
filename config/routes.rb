fb518e566ee19173e6d096afd2529a64dc3bbcb4
KamizatoR <112645912+kamizatoR@users.noreply.github.com>
2022-09-16 07:00:26 +0000

    [Finish]4-14

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'homes#top'
-
+  get 'homes/about' => 'homes#about',as: 'about'
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
   resources :post_images, only: [:new, :index, :show, :create, :destroy]
-
-  get 'homes/about' => 'homes#about',as: 'about'
+  resources :users, only[:show, :edit]
+  
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
