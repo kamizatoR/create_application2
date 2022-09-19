708ac76f0c9899241204e1f15754ade8e83584a2
KamizatoR <112645912+kamizatoR@users.noreply.github.com>
2022-09-19 14:49:12 +0000

    [Finish]4-16~4-21

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,12 +2,15 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
-  
+
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+     if @post_image.save
+       redirect_to post_image_path
+     else
+       render :new
+     end
   end
 
   def index
@@ -16,16 +19,18 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
+
   end
-  
+
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
     redirect_to post_images_path
   end
-  
+
   private
-  
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :caption, :image)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
 
+
   devise_for :users
-  root to: 'homes#top'
-  get 'homes/about' => 'homes#about',as: 'about'
-  # get 'post_images/new'
-  # get 'post_images/index'
-  # get 'post_images/show'
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
-  resources :users, only: [:show, :edit]
-  
-  
-  
+  root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+
+    resource :favorites, only: [:create, :destroy]
+    
+  end
+  resources :users, only: [:show, :edit, :update]
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
